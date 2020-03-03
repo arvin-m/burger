4c73a7d7b8cd41e104c6a24c7584ca6bae7528f3
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-03-03 14:45:31 -0500

    creating file and folder for project

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const connection=require("mysql");
+Inside the connection.js file, setup the code to connect Node to MySQL.
+
+Export the connection.
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports=connection ;
