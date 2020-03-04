f66d1d683121385132eabc7ed3916874af91cdf1
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-03-04 15:19:18 -0500

    adding code to handel the main handelbars

--- server.js
+++ server.js
@@ -1,2 +1,34 @@
 const express=require("express");
+let exphbs = require("express-handlebars");
+const app=express();
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
 const PORT =process.env.PORT||8020;
+
+app.use(express.static("public"));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT,(err)=>{
+    if (err) throw err;
+
+    console.log(`You Are Connecting To The Port localhost http://localhost:${PORT}/`);
+
+});
